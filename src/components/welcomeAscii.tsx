bcf5872acad8711d08068ad8e88a3600755305f5
Prajwal Aradhya <manuaradhya07@gmail.com>
2026-01-28 19:46:09 +0000

    fixed ASCII

--- src/components/welcomeAscii.tsx
+++ src/components/welcomeAscii.tsx
@@ -4,14 +4,14 @@ import ascii from "../ascii.txt";
 // https://patorjk.com/software/taag/#p=display&f=Rectangles&t=P+R+A+J+W+A+L%0A++W+e+l+c+o+m+e+s%0A+++++++++Y+O+U&x=none&v=4&h=4&w=80&we=false
 const WelcomeAscii = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <pre className="text-left">{ascii}</pre>
       <br />
       <p className="text-sm lg:text-xl">
         Welcome to my portfolio <br /> A terminal-style navigation experience
       </p>
       <br />
-      <p className="text-sm lg:text-xl">
+      <p className="text-sm lg:text-xl lg:max-w-[70%]">
         I’m a software engineer and developer with a passion for building
         scalable systems, experimenting with new technologies, and sharing what
         I learn along the way.
